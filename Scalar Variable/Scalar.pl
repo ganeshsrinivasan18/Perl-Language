94a379eccf5526a9d66d1a5014cbf4e7855af0e8
Ganesh <ganesheart1@gmail.com>
2025-04-18 21:22:20 +0530

    Create Scalar.pl

---
+++ Scalar Variable/Scalar.pl
@@ -0,0 +1,9 @@
+#Defining Scalar variable
+
+$student_name = "Ganesh";
+$student_age = 23;
+$student_cgpa = 8.15;
+
+print "Age = $student_age\n";
+print "Name = $student_name\n";
+print "CGPA = $student_cgpa\n";
