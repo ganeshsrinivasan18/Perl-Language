bbd8f6fc91f4de5164f30086371d68d47ff0f026
Ganesh <ganesheart1@gmail.com>
2025-04-18 21:24:06 +0530

    Create Array.pl

---
+++ Array Variable/Array.pl
@@ -0,0 +1,15 @@
+#Defining Array variable
+
+@name = ("Jensen","Ganesh","Haviv");
+
+@age = (62,21,56);
+
+#Printing Names
+print "\$name[0] = $name[0]\n";
+print "\$name[1] = $name[1]\n";
+print "\$name[2] = $name[2]\n";
+
+#Printing Ages
+print "\$age[0] = $age[0]\n";
+print "\$age[1] = $age[1]\n";
+print "\$age[2] = $age[2]\n";
