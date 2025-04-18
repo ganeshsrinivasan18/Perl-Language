735d9b8316ae43d4f7d293126bcc244f4c49ed0e
Ganesh <ganesheart1@gmail.com>
2025-04-18 21:26:17 +0530

    Create Hashes.pl

---
+++ Hashes Variable/Hashes.pl
@@ -0,0 +1,7 @@
+#Defining Hashes variable
+
+%student_data = ('Ganesh',21,'Jensen',62,'Haviv',56);
+
+print "\$student_data{'Ganesh'} = $student_data{'Ganesh'}\n";
+print "\$student_data{'Jensen'} = $student_data{'Jensen'}\n";
+print "\$student_data{'Haviv'} = $student_data{'Haviv'}\n";
