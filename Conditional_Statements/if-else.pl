1e6c13c7cd62f2bbba7dcb9fe922844a4bc07811
Ganesh <ganesheart1@gmail.com>
2025-04-22 15:09:38 +0530

    Create if-else.pl

---
+++ Conditional_Statements/if-else.pl
@@ -0,0 +1,15 @@
+# Conditional Statements in  Perl
+
+#1. if-else condition in perl
+
+$salary = 10500;
+
+if ($salary > 10000) { 
+    print("Employee is Manager\n");
+}
+    
+    elsif ($salary<10000) {
+        print("Employee is Staff\n")
+    }
+    
+
